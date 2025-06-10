0c68ef2256947e87c3d993aedabbe8dbb6cae023
Rabindra Tamang <rabindra.tamang@g-mana.com>
2025-06-10 23:10:21 +0545

    bugfix/youtube-nb: Move config.py

--- backend/core/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-JWT_SECRET = os.getenv("JWT_SECRET", "supersecret")
-JWT_ALGORITHM = "HS256" 

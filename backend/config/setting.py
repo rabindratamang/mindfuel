c2ba2a49144c549332a88add6743842c8ad37eb9
Shashwot90 <shashwotpradhan@gmail.com>
2025-06-11 22:10:16 +0545

    spotify client id and secret

--- backend/config/setting.py
+++ backend/config/setting.py
@@ -4,3 +4,5 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
 JWT_SECRET = os.getenv("JWT_SECRET", "supersecret")
 JWT_ALGORITHM = "HS256" 
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")

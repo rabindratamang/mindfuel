d8afe054ffd7016596741fb5262e74299ba92035
Rabindra Tamang <rabindra.tamang@g-mana.com>
2025-06-15 01:26:12 +0545

    feature/database: Add database handler with api lifespan

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from api import auth, agent
+from config.database import mongodb_obj
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await mongodb_obj.connect_db()
+    yield
+    await mongodb_obj.close_db()
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(agent.router, tags=["agent"])
 

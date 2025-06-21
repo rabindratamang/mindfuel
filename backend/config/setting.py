dcd8c2443cd3999d782cf40908db8a27385c1525
Rabindra Tamang <rabindra.tamang@g-mana.com>
2025-06-21 12:51:52 +0545

    enhance/mood-analysis: Update settings and remove old database approach

--- backend/config/setting.py
+++ backend/config/setting.py
@@ -2,14 +2,15 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     OPENAI_API_KEY: str
-    RAPIDAPI_KEY: str
+    RAPID_API_KEY: str
     JWT_SECRET: str = "supersecret"
     JWT_ALGORITHM: str = "HS256"
-    CLIENT_ID: str
-    CLIENT_SECRET: str
+    SPOTIFY_CLIENT_ID: str
+    SPOTIFY_CLIENT_SECRET: str
     SENDGRID_API_KEY: str
     MONGODB_URL: str
     DATABASE_NAME: str
+    MONGODB_DB_NAME: str
 
     class Config:
         env_file = ".env"
